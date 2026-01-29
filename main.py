9018a2bcdeb58d65ee0667fbff4531723ed027f3
Echo <echorw927@gmail.com>
2026-01-28 21:00:44 -0800

    d

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ bot = commands.Bot(
 
 @bot.event
 async def on_ready():
-    print(Fore.LIGHTWHITE_EX + f'finished startup - now running')
     print(Fore.LIGHTWHITE_EX + f'logged in as {bot.user.name} - {bot.user.id} ({discord.__version__})')
     print(Fore.RESET)
     bot.startup_time = discord.utils.utcnow()
