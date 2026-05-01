fa0b866095c0b5e2d521f8387e829ea48203499f
Echo <echorw927@gmail.com>
2026-04-30 18:56:06 -0700

    s

--- cogs/okemollm.py
+++ cogs/okemollm.py
@@ -30,7 +30,7 @@ v_client = AsyncOpenAI(
 
 client = AsyncOpenAI(
     api_key="sk-none",
-    base_url="https://api.okemovail.com"
+    base_url="https://api.okemovail.com/v1/"
 )
 
 olm_is = ["is thinking", "is cooking up a response", "wants you to order a Shamrock Shake® from McDonalds for only $5.19", "is crunching data", "thinks you're dumb", "thinks 1+1 equals ~~4~~ 2", "is making Minecraft 2", "is not real", "is <UNK><UNK><UNK><UNK><UNK>", "is probably not claude", "is typing on virtual keyboards", "is 100% artificial", "is baking 3,000 cookies", "thinks you should buy 30,000,000 Robux", "is consuming more RAM", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "will respond very good", "meows cutely", "i okemollm okemollm, and glittery okemollm, glittery glittery storm glittery storm okemollm glittery storm okemollm glittery storm okemollm", "can almost write a haiku", "is counting sheep", "aaaaaaaaaaaaaa", "is miwi certified", "is hallucinating excel spreadsheets", "dreams of kiwis", "- powered by 13 oceans", "2: electric boogaloo", "is migrating to mars"]
