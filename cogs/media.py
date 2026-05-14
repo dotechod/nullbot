04422dcc92d2e138a99ba20c24ea050964cfd530
Echo <echorw927@gmail.com>
2026-05-13 20:01:36 -0700

    s

--- cogs/media.py
+++ cogs/media.py
@@ -145,6 +145,108 @@ class Media(commands.Cog):
         song_cache.clear()
         await ctx.reply("Cache cleared.")
 
+    @commands.hybrid_command(name="song")
+    async def song_cmd(self, message, link):
+        try:
+            if match := self.pattern.search(message.content.strip("<>")):
+                link = match.group(0).split("?")[0]
+                if link in song_cache:
+                    song = song_cache[link]
+                else:
+                    song = requests.get(f"https://api.song.link/v1-alpha.1/links?url={link}")
+                    song_cache[link] = song
+                info = song.json()
+
+                if not "entityUniqueId" in info: return
+                songid = info["entityUniqueId"]
+                title = info["entitiesByUniqueId"][songid]["title"]
+                author = info["entitiesByUniqueId"][songid]["artistName"]
+                thumbnail = info["entitiesByUniqueId"][songid]["thumbnailUrl"]
+                link_type = info["entitiesByUniqueId"][songid]["type"]
+
+                if "youtube" in match.group(0) or "youtu.be" in match.group(0):
+                    if 'spotify' not in info.get('linksByPlatform', {}):
+                        return
+
+                lastfm_data = requests.get("http://ws.audioscrobbler.com/2.0/",
+                                           params={
+                                               'method': 'track.getInfo',
+                                               'api_key': os.getenv("LASTFM"),
+                                               'artist': author,
+                                               'track': title,
+                                               'format': 'json'
+                                           },
+                                           headers={
+                                               'User-Agent': os.getenv("LASTFM_AGENT")
+                                           }
+                                           )
+
+                if lastfm_data.status_code == 200:
+                    lfm_data = lastfm_data.json()
+                    if 'track' in lfm_data:
+                        print(lfm_data)
+                        playcount = int(lfm_data['track']['playcount'])
+                        listeners = int(lfm_data['track']['listeners'])
+                    else:
+                        playcount = 0
+                        listeners = 0
+                else:
+                    playcount = 0
+                    listeners = 0
+
+                view = discord.ui.LayoutView()
+                container = discord.ui.Container()
+
+                text_content = f"**{author} - {title}**"
+                if link_type == "album":
+                    text_content += "\nAlbum"
+                if playcount != 0:
+                    text_content += f"\n{listeners} listeners | {playcount} plays"
+
+                section = discord.ui.Section(
+                    discord.ui.TextDisplay(text_content),
+                    accessory=discord.ui.Thumbnail(thumbnail)
+                )
+                container.add_item(section)
+
+                container.add_item(discord.ui.Separator(visible=True))
+
+                action_row = discord.ui.ActionRow()
+                action_row.add_item(discord.ui.Button(url=info['pageUrl'], emoji="<:export:1464509598101934379>",
+                                                      label="View on song.link"))
+                container.add_item(action_row)
+
+                action_row = discord.ui.ActionRow()
+                if "spotify" in info["linksByPlatform"]:
+                    spotify = info["linksByPlatform"]["spotify"]["url"]
+                    action_row.add_item(discord.ui.Button(url=spotify, emoji="<:spotify:1464513267794968704>"))
+                if "appleMusic" in info["linksByPlatform"]:
+                    am = info["linksByPlatform"]["appleMusic"]["url"]
+                    action_row.add_item(discord.ui.Button(url=am, emoji="<:applemusic:1464513246009753727>"))
+                if "youtubeMusic" in info["linksByPlatform"]:
+                    ytm = info["linksByPlatform"]["youtubeMusic"]["url"]
+                    action_row.add_item(discord.ui.Button(url=ytm, emoji="<:ytm:1464513257896411302>"))
+                if "youtube" in info["linksByPlatform"]:
+                    yt = info["linksByPlatform"]["youtube"]["url"]
+                    action_row.add_item(discord.ui.Button(url=yt, emoji="<:yt:1464513278230138880>"))
+                container.add_item(action_row)
+
+                view.add_item(container)
+
+                await message.edit(suppress=True)
+                await message.reply(view=view, mention_author=False)
+
+        except:
+            c_id = data[message.guild.id]["server_log"]  # server logging channel
+            ch = self.bot.get_channel(c_id)
+            log = traceback.format_exc()
+            traceback.print_exc()
+            await ch.send(f"## big sad happened"
+                          f"```{log}```")
+            await message.reply(f"```{log}```", delete_after=5)
+
+
+
     @commands.command()
     async def linksong(self, ctx):
         try:
